75a9f0d9184a368821e63c0d3e49c384ff43dec7
Shivani Chahal <shivanichahal@Shivanis-MacBook-Air.local>
2026-01-03 23:24:23 -0800

    additional sql commit

--- sql-script/DataQuality.sql
+++ sql-script/DataQuality.sql
@@ -6,3 +6,17 @@ SELECT
     COUNT(CASE WHEN "RatecodeID" NOT IN (1,2,3,4,5,6,99) THEN 1 END) as bad_rate_codes,
     COUNT(CASE WHEN "fare_amount" < 0 THEN 1 END) as negative_fares
 FROM "training-database"."transformed-yellow_taxi_enriched";
+
+
+--- Additional checks can be added as needed
+
+SELECT * FROM "training-database"."yellow_tripdata_2025_08" limit 100;
+
+select count(1) from "training-database"."yellow_tripdata_2025_08".  ---3574091
+
+
+select *  from "training-database"."yellow_tripdata_2025_08" where RatecodeID is null limit 10;
+
+select *  from "training-database"."yellow_tripdata_2025_08" where PULocationID = DOLocationID ;
+
+select distinct  DOLocationID  from "training-database"."yellow_tripdata_2025_08" where DOLocationID not in (SELECT locationid FROM "training-database"."taxi_zone_lookup")
